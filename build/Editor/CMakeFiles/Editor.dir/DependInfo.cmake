c74e6b44ea882cc7c34133e2d5a389ea05def323
byosss <karimmuckensturm8a@gmail.com>
2024-12-15 12:19:56 +0100

    Update .gitignore to ignore build directories and add generated CMake files

---
+++ build/Editor/CMakeFiles/Editor.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Karim/Desktop/code/C-C++/THE3DENGINE/Editor/OpenGLWindow.cpp" "Editor/CMakeFiles/Editor.dir/OpenGLWindow.cpp.obj" "gcc" "Editor/CMakeFiles/Editor.dir/OpenGLWindow.cpp.obj.d"
+  "C:/Users/Karim/Desktop/code/C-C++/THE3DENGINE/Editor/main.cpp" "Editor/CMakeFiles/Editor.dir/main.cpp.obj" "gcc" "Editor/CMakeFiles/Editor.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
