b31b1d0912b06b1cc207a3ae74c7a634ada80274
byosss <karimmuckensturm8a@gmail.com>
2025-02-22 19:09:21 +0100

    import assimp lib

--- Engine/src/Resource/Model.h
+++ Engine/src/Resource/Model.h
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include "Resource.h"
 #include "Mesh.h"
 
@@ -10,8 +14,17 @@ class Model : public Resource {
 public:
     explicit Model(const std::string& path) {
         // Load model from file
-        // ...
-        std::cout << "Chargement du model 3d : " << path << std::endl;
+        Assimp::Importer importer;
+
+        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+            std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+            return;
+        }
+
+        // Process ASSIMP's root node recursively
+        processNode(scene->mRootNode, scene);
     }
     ~Model() {
         // Release model data
@@ -20,5 +33,25 @@ public:
     }
 
 private:
+    void processNode(aiNode* node, const aiScene* scene) {
+        // Process each mesh located at the current node
+        for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            m_meshes.push_back(processMesh(mesh));
+        }
+
+        // Process each child node
+        for (unsigned int i = 0; i < node->mNumChildren; i++) {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    Mesh processMesh(aiMesh* mesh) {
+        
+        // Mesh mesh(mesh->mVertices, mesh->mNumVertices, 
+
+        
+    }
+
     std::vector<Mesh> m_meshes;
 };
